8e9a2f38bab962c2a55daf8dcf666362c390fef6
feuerstern <rio-kurniawan@sakura-schutzstaffel.com>
2024-10-15 13:37:17 +0700

    solved the repeat a string problem

--- confirm-the-ending/index.js
+++ confirm-the-ending/index.js
@@ -3,4 +3,14 @@ function confirmEnding(str, target) {
 }
 const t = confirmEnding("Abstraction", "action");
 
-console.log(t);
+// console.log(t);
+
+const findMaxValue = (arr) => {
+  arr.sort((a, b) => {
+    return b - a;
+  });
+};
+
+const arr = [203, 100, 40, 3, 1031, 1998, 233, 111]
+console.log(findMaxValue(arr));
+console.log(arr[0])
