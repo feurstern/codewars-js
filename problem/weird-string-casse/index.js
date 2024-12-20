967a814e399dc4b6e43efbbf68b05b651f3ec7a3
feuerstern <rio-kurniawan@sakura-schutzstaffel.com>
2024-12-20 14:09:54 +0700

    Solved the weird-string-case problem

---
+++ problem/weird-string-casse/index.js
@@ -0,0 +1,17 @@
+const t = "hatsune miku is so gorgeous xixixi";
+const strConvert = (str) => {
+  let result = [];
+
+  str.split(" ").forEach((x) => {
+    x.split("").forEach((z, i) => {
+      const index = i + 1;
+      index % 2 == 1
+        ? result.push(z.toUpperCase())
+        : result.push(z.toLowerCase());
+    });
+    result.push(" ");
+  });
+  return result.join("").toString();
+};
+
+console.log(strConvert("This is a test"));
