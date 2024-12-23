61427302680d6847de6ccd6e93b5e7e5651f1576
feuerstern <rio-kurniawan@sakura-schutzstaffel.com>
2024-12-23 13:25:19 +0700

    implemeted linked list by using the class

---
+++ problem/get-the-loop/LinkedList.js
@@ -0,0 +1,35 @@
+class ListNode {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+  size() {
+    let count = 0;
+    let node = this.data;
+    while (node) {
+      count++;
+      node = node.next;
+    }
+    return count;
+  }
+  clear() {
+    this.next = null;
+  }
+  getFirst() {
+    return this.next;
+  }
+}
+
+let node1 = new ListNode(2);
+const node2 = new ListNode(10);
+node1.next = node2;
+node1.next = node2;
+
+const list = new ListNode(node1);
+list.next = node1;
+
+console.log("size of the linked list:", list.size());
+console.log("first linked list:", list.getFirst());
+console.log("node list", list);
+list.clear();
+console.log("node list after", list);
